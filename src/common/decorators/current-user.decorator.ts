c6dac6278cfadf281c51ed34c2a94571c2300f77
Djuradj Djuric <djura2707@gmail.com>
2026-04-09 17:23:43 +0200

    remove unnecessary coe

--- src/common/decorators/current-user.decorator.ts
+++
@@ -1,9 +0,0 @@
-import { createParamDecorator, ExecutionContext } from "@nestjs/common";
-import type { AuthenticatedUser } from "../types/auth-user";
-
-export const CurrentUser = createParamDecorator(
-  (_data: unknown, ctx: ExecutionContext): AuthenticatedUser | undefined => {
-    const req = ctx.switchToHttp().getRequest();
-    return req.authUser as AuthenticatedUser | undefined;
-  },
-);
